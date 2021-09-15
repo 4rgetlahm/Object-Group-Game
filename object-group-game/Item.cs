ee63c54c91add7b10c4dfbf5e3151e3d3a39723a
martynas.slav <martynas.slav@gmail.com>
2021-09-15 23:54:20 +0300

    Add Item class.

---
+++ object-group-game/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_group_game {
+	class Item {
+		private string name;
+		private List<Effect> effects;
+
+		Item(string name, List<Effect> effects) {
+			this.name = name;
+			this.effects = effects;
+		}
+
+		string GetName() {
+			return name;
+		}
+
+		List<Effect> GetEffects() {
+			return effects.clone();
+		}
+	}
+}
