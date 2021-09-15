8f5dffb2f447e59856fe3c0edb0d9e76c3fe3f41
arnasv <iwoops12@gmail.com>
2021-09-15 17:01:46 +0300

    Add Location object, LocationType enum with basic types.

---
+++ object-group-game/LocationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_group_game
+{
+    enum LocationType
+    {
+        FOREST,
+        PLAINS,
+        DUNES,
+        SEA,
+        OCEAN,
+        RIVER,
+        LAKE,
+        HOLY,
+        DARK
+    }
+}
