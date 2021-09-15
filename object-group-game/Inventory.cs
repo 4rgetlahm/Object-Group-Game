0db5a4bb7aa12f11029839c0a81c21caf622ff4d
martynas.slav <martynas.slav@gmail.com>
2021-09-15 23:54:11 +0300

    Add Inventory class.

---
+++ object-group-game/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_group_game {
+	class Inventory {
+		private List<Item> items;
+
+		List<Item> GetItems() {
+			return items.clone();
+		}
+	}
+}
