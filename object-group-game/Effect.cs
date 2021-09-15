ab5e905f581c09804d80b116b77ea86df332bb7a
martynas.slav <martynas.slav@gmail.com>
2021-09-15 23:53:58 +0300

    Add Effect class.

---
+++ object-group-game/Effect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace object_group_game {
+	class Effect {
+		private string status;
+		private string displayName;
+		private int value;
+
+		Effect(string status, string displayName, int value) {
+			this.status = status;
+			this.displayName = displayName;
+			this.value = value;
+		}
+
+		string GetEffectKey() {
+			return status;
+		}
+
+		string GetDisplayName() {
+			return displayName;
+		}
+
+		int GetValue() {
+			return value;
+		}
+	}
+}
