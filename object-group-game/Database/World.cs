f4f9f0fdfbef9da35c83e632aa38aa70a01b2cb6
martynas.slav <martynas.slav@gmail.com>
2021-10-06 21:49:33 +0300

    Fix World class. Change formatting. Fix issue with multiple SQL DataReaders. Add return null to GetItem(int) function.

--- object-group-game/Database/World.cs
+++ object-group-game/Database/World.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace object_group_game
 {
 	public class World
 	{
 		// Returns all locations from the database
-		public static List<Location> GetLocations()
-		{
-			using(var context = new Database.WorldContext()) 
-			{
-				List<Location> locationList = new List<Location>();
-
+		public static List<Location> GetLocations() {
+			using (var context = new Database.WorldContext()) {
 				var locations = context.Location;
 
-				foreach (var location in locations)
-				{
+				List<Location> locationList = new List<Location>();
+
+				foreach (var location in locations) {
 					var item = GetItem(location.ItemID);
 
 					locationList.Add(new Location(location, item));
@@ -28,41 +22,42 @@ namespace object_group_game
 			}
 		}
 
-		// Returns item with provided ID
-		public static Item GetItem(int id)
-		{
-			using(var context = new Database.WorldContext())
-			{
-				var item = context.Item.Where(i => i.ID == id).FirstOrDefault();	
+		// Returns item with provided ID if exists
+		// Returns null otherwise
+		public static Item GetItem(int id) {
+			using (var context = new Database.WorldContext()) {
+				var item = context.Item.Where(i => i.ID == id).First();
 
-				List<Effect> effects = new List<Effect>();
-
-				var itemEffects = context.ItemEffects.Where(e => e.ItemID == id);
-
-				foreach (var itemEffect in itemEffects) {
-					Effect effect = context.Effect.Where(e => e.ID == itemEffect.EffectID).FirstOrDefault();
-
-					effect.Value = itemEffect.Value;
-
-					effects.Add(effect);
+				if (item == null) {
+					return null;
 				}
+				else {
+					List<Effect> effects = new List<Effect>();
 
-				return new Item(item, effects);
+					var itemEffects = context.ItemEffects.Where(e => e.ItemID == id).ToList();
+
+					foreach (var itemEffect in itemEffects) {
+						Effect effect = context.Effect.Where(e => e.ID == itemEffect.EffectID).First();
+
+						effect.Value = itemEffect.Value;
+
+						effects.Add(effect);
+					}
+
+					return new Item(item, effects);
+				}
 			}
 		}
 
 		// Returns all items from the database
-		public static List<Item> GetItems() 
-		{
-			using(var context = new Database.WorldContext())
-			{
+		public static List<Item> GetItems() {
+			using (var context = new Database.WorldContext()) {
 				var items = context.Item;
 
 				List<Item> itemList = new List<Item>();
 
-				foreach (var item in items)
-				{
-					itemList.Add(GetItem(item.ID));	
+				foreach (var item in items) {
+					itemList.Add(GetItem(item.ID));
 				}
 
 				return itemList;
