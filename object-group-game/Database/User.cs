e832dab37addb56e1f310246cad1fa872f4492d3
martynas.slav <martynas.slav@gmail.com>
2021-10-06 22:14:06 +0300

    Make Database.User class methods static.

--- object-group-game/Database/User.cs
+++ object-group-game/Database/User.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace object_group_game.Database
 {
 	class User
 	{
 		// Checks whether a login with provided username exists
-		public bool UserExists(string username)
+		public static bool UserExists (string username)
 		{
-			using(var context = new Database.UserContext())
+			using (var context = new Database.UserContext())
 			{
 				return context.Login.Where(l => l.Username.Equals(username)).FirstOrDefault() != null;
 			}
 		}
 
 		// Checks whether there is a login with provided username and password
-		public bool CorrectPassword(string username, string password)
+		public static bool CorrectPassword (string username, string password)
 		{
-			using(var context = new Database.UserContext())
+			using (var context = new Database.UserContext())
 			{
 				return context.Login.Where(l => l.Username.Equals(username) && l.Password.Equals(password)).FirstOrDefault() != null;
 			}
@@ -28,9 +24,9 @@ namespace object_group_game.Database
 
 		// Creates a new login inside the database
 		// Assumes the username is checked for availability
-		public void NewLogin(string username, string password)
+		public static void NewLogin (string username, string password)
 		{
-			using(var context = new Database.UserContext())
+			using (var context = new Database.UserContext())
 			{
 				context.Login.Add(new Login() {
 					Username = username,
