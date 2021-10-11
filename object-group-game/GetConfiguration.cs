b485613ce5d16010d6a652f3f2742ebb7f51fa39
Eimantas <76561385+eimanteras@users.noreply.github.com>
2021-10-11 20:30:31 +0300

    Create App.config | Create GetConfig.cs class

---
+++ object-group-game/GetConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_group_game
+{
+    public class GetConfiguration
+    {
+        public static void GetConfigurationValue()
+        {
+            string connect = ConfigurationManager.AppSettings["ConnectionString"];
+            int MaxItems = Int16.Parse(ConfigurationManager.AppSettings["MaxItems"]);
+            int MaxGold = Int16.Parse(ConfigurationManager.AppSettings["MaxItems"]);
+        }
+    }
+}
