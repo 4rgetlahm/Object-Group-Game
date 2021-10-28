9890d5e85a7d1cb6cfa40e407672f74684bac050
arnasv <iwoops12@gmail.com>
2021-10-28 15:37:10 +0300

    Character and Player Events

--- object-group-game/Player/Character.cs
+++ object-group-game/Player/Character.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using object_group_game.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace object_group_game
 {
@@ -13,13 +15,75 @@ namespace object_group_game
     {
         [Key]
         public int CharacterID { get; set; }
-        public string Name { get; private set; }
-        public double Health { get; set; }
-        public double Experience { get; set; }    
-        public double Mana { get; set; }
-        public double Gold { get; set; }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnCharacterUpdate(new CharacterEventArgs(this));
+            }
+        }
+        private double _health;
+        public double Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                _health = value;
+                OnCharacterUpdate(new CharacterEventArgs(this));
+            }
+        }
+        private double _experience;
+        public double Experience
+        {
+            get
+            {
+                return _experience;
+            }
+            set
+            {
+                _experience = value;
+                OnCharacterUpdate(new CharacterEventArgs(this));
+            }
+        }
+        private double _mana;
+        public double Mana
+        {
+            get
+            {
+                return _mana;
+            }
+            set
+            {
+                _mana = value;
+                OnCharacterUpdate(new CharacterEventArgs(this));
+            }
+        }
+        private double _gold;
+        public double Gold { 
+            get 
+            {
+                return _gold;    
+            }
+            set
+            {
+                _gold = value;
+                OnCharacterUpdate(new CharacterEventArgs(this));
+            }
+        }
         public virtual ICollection<Item> Items { get; set; }
         public virtual ICollection<Location> VisitedLocations { get; set; }
+
+        public delegate void CharacterUpdateEventHandler(CharacterEventArgs args);
+        public event CharacterUpdateEventHandler CharacterUpdateEvent;
 
         protected Character()
         {
@@ -85,6 +149,14 @@ namespace object_group_game
                 totalIntelligence += item.Intelligence;
             }
             return totalIntelligence;
+        }
+
+        public void OnCharacterUpdate(CharacterEventArgs args)
+        {
+            if (CharacterUpdateEvent != null)
+            {
+                CharacterUpdateEvent(args);
+            }
         }
 
     }
